b9599648bf916ac1fd098c0e45cb549a26da5fb0
fakechris <chris__song@hotmail.com>
2008-02-19 07:30:00 +0000

    is_basic_latin worked --> only support unicode object

--- pychseg/extend/charutils.c
+++ pychseg/extend/charutils.c
@@ -5,24 +5,24 @@
 
 static PyObject* is_basic_latin(PyObject* self, PyObject* args) 
 { 
-	PyObject *result;
 	Py_UNICODE  *src;
-	int srclen, i;
+	int srclen;
+	register Py_UNICODE ch;
 
-	if (! PyArg_ParseTuple(args, "u", &src))
+	if (! PyArg_ParseTuple(args, "u#", &src, &srclen))
 		return NULL;
 
-	srclen = strlen(src);
-	assert(srclen == 1);
-	
-	//i = ord(src);
-	
-	printf("%d\n", srclen);
-	
-	Py_RETURN_TRUE; 
-	
-} 
+	if (srclen< 1) {
+		return PyBool_FromLong(0);
+	}
 
+	ch = *src;
+		
+	if ( (ch >= 0x20 && ch <=0x7f) ) /* || (ch >= 0xff01 && ch <= 0xff5e) ) */	
+		return PyBool_FromLong(1);
+	else
+		return PyBool_FromLong(0);	
+} 
 
 static PyMethodDef charutilsMethods[] =
 { 
